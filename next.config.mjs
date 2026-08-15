8828fdb41762600234aa00ef30d6e5d021570f9d
angelguajardo <41927488+angelguajardo@users.noreply.github.com>
2026-08-15 14:19:49 -0400

    Allow provider listing image hosts

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,36 @@ const nextConfig = {
         hostname: "cdn.shopify.com",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "www.nychdc.com",
+        pathname: "/sites/default/files/**",
+      },
+      {
+        protocol: "https",
+        hostname: "nychdc.com",
+        pathname: "/sites/default/files/**",
+      },
+      {
+        protocol: "https",
+        hostname: "residenewyork.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "www.residenewyork.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "fifthave.org",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "www.fifthave.org",
+        pathname: "/**",
+      },
     ],
   },
 };
